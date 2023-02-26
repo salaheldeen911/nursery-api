eab133eaab4051463c16bc32a5844681ffebcfd6
salah <salah.eldeen.mail@gmail.com>
2023-02-26 15:04:47 +0200

    updated

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
@@ -11,9 +13,10 @@ class UserController extends Controller
     public function update(UpdateUserRequest $request)
     {
         try {
-            auth('sanctum')->user()->update($request->validated());
+            $user = User::with('roles')->find(auth('sanctum')->id());
+            $user->update($request->validated());
 
-            return $this->_response(self::SUCCESS, "users data has been updated successfully");
+            return $this->_response(self::SUCCESS, "users data has been updated successfully", new UserResource($user));
         } catch (\Exception $e) {
             Log::error("error while storing an suggestions", ['error_msg' => $e->getMessage() . "in line: " . $e->getLine(), "trace" => $e->getTraceAsString()]);
 
